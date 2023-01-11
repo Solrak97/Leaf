eb9c11a9220e21710dff783977b4299b03d4d2e5
solrak <luiscarlosquesada@gmail.com>
2023-01-10 23:05:39 -0600

    DisplayManager is not a masterpiece but it works!
    Display manager can create and destroy windows, still not abled to update it but im on it

--- src/Leaf/MainGameLoop.java
+++ src/Leaf/MainGameLoop.java
@@ -1,8 +1,45 @@
 package Leaf;
 
+
+import RenderEngine.DisplayManager;
+import org.lwjgl.opengl.*;
+
+import static org.lwjgl.glfw.GLFW.*;
+import static org.lwjgl.opengl.GL11.*;
+
 public class MainGameLoop {
 
-    public static void main(string[] args) {
-        DisplayManager
+    public static void main(String[] args) {
+        if (!glfwInit())
+            throw new IllegalStateException("Unable to initialize GLFW");
+        long window = DisplayManager.createDisplay("LEAF");
+
+        GL.createCapabilities();
+
+        glClearColor(0f, 0f, 0f, 0.0f);
+        while (!glfwWindowShouldClose(window)) {
+            glfwPollEvents();
+            glClear(GL_COLOR_BUFFER_BIT);
+
+            glBegin(GL_QUADS);
+
+                glColor4f(1, 0, 0, 1);
+                glVertex2d(-0.5, 0.5);
+
+                glColor4f(0, 1, 0, 1);
+                glVertex2d(0.5, 0.5);
+
+                glColor4f(0, 0, 1, 1);
+                glVertex2d(0.5, -0.5);
+
+                glColor4f(1, 1, 1, 1);
+                glVertex2d(-0.5, -0.5);
+
+            glEnd();
+
+            glfwSwapBuffers(window);
+        }
+
+        glfwTerminate();
     }
 }
--- src/RenderEngine/DisplayManager.java
+++ src/RenderEngine/DisplayManager.java
@@ -1,25 +1,64 @@
 package RenderEngine;
 
-import org.lwjgl.LWJGLException;
-import org.lwjgl.opengl.Display;
-import org.lwjgl.opengl.ContextAttribs;
-import org.lwjgl.opengl.DisplayMode;
-import org.lwjgl.opengl.GL11;
-import org.lwjgl.opengl.PixelFormat;
+import org.lwjgl.glfw.*;
+import org.lwjgl.system.*;
+
+import java.nio.*;
+
+import static org.lwjgl.glfw.Callbacks.*;
+import static org.lwjgl.glfw.GLFW.*;
+import static org.lwjgl.system.MemoryStack.*;
+import static org.lwjgl.system.MemoryUtil.*;
 
 public class DisplayManager {
-    private static final int WIDTH = 120;
+    private static final int WIDTH = 1280;
     private static final int HEIGHT = 720;
     private static final int FPS_CAP = 120;
 
+    private static long window;
 
-    public static void createDisplay() {
+    public static long createDisplay(String title) {
+        glfwDefaultWindowHints();
+        glfwWindowHint(GLFW_VISIBLE, GLFW_FALSE);
+        glfwWindowHint(GLFW_RESIZABLE, GLFW_TRUE);
+        window = glfwCreateWindow(WIDTH, HEIGHT, title, NULL, NULL);
+        if (window == NULL) {
+            throw new RuntimeException("Failed to create GLFW Window");
+        }
 
-    }
+        // this function should be passed as a lambda from the first level function
+        glfwSetKeyCallback(window, (window, key, scancode, action, mods) -> {
+            if (key == GLFW_KEY_ESCAPE && action == GLFW_RELEASE)
+                glfwSetWindowShouldClose(window, true);
+        });
+
+        try (MemoryStack stack = stackPush()) {
+            IntBuffer pWidth = stack.mallocInt(1);
+            IntBuffer pHeight = stack.mallocInt(1);
+
+            glfwGetWindowSize(window, pWidth, pHeight);
+
+            GLFWVidMode vidmode = glfwGetVideoMode(glfwGetPrimaryMonitor());
+
+            glfwSetWindowPos(
+                    window,
+                    (vidmode.width() - pWidth.get(0)) / 2,
+                    (vidmode.height() - pHeight.get(0)) / 2
+            );
+        }
+
+        glfwMakeContextCurrent(window);
+        glfwSwapInterval(1);
+        glfwShowWindow(window);
+
+        return window;
+}
 
     public static void updateDisplay() {
     }
 
     public static void closeDisplay() {
+        glfwFreeCallbacks(window);
+        glfwDestroyWindow(window);
     }
 }
